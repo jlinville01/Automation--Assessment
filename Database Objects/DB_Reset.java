4cdcaa2baa8935633d3b1e4c1432c81afc77e160
Justin Linville <jlinville01@gmail.com>
2016-12-02 10:28:34 -0800

    Update DB_Reset.java

--- Database Objects/DB_Reset.java
+++ Database Objects/DB_Reset.java
@@ -1,10 +1,7 @@
-/*
- * Database methods for resetting accounts
- */
-
 package database;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.openqa.selenium.WebDriver;
@@ -16,10 +13,21 @@ import page_objects.StandOut;
 public class DB_Reset extends StandOut
 {
 	private Statement stmt;
+	private ResultSet result = null;
+	private ResultSet dA = null;
+	private ResultSet cA = null;
+	private ResultSet aR = null;
 	
 	public void runTest() throws Exception {};
 	public void tearDown() throws Exception {};
 	
+	/**
+	 * Instantiates the DB_Reset page object.
+	 * 
+	 * @param driver	the driver for this class.
+	 * @param connection	the db connection for this class.
+	 * @throws Exception
+	 */
 	public DB_Reset(WebDriver driver, java.sql.Connection connection) throws Exception 
 	{
 		PageFactory.initElements(driver, this);
@@ -34,21 +42,23 @@ public class DB_Reset extends StandOut
 	 * @param email		the mailbox for the accounts to be reset
 	 * @param endEmail	the domain name for the accounts to be reset
 	 * @param clientID	the client id for the account to be reset
+	 * @throws SQLException 
 	 */
-	public void resetPersona(String email, String endEmail, String clientID)
+	public void resetPersona(String email, String endEmail, String clientID) throws SQLException
 	{
 		try
-	    {
-	      stmt = con.createStatement();
-	      String runReset = "CALL strengthaccelerator.Proc_ResetPersonaAccounts('" + email + "', '" + endEmail + "', '" + clientID + "');";
-	      ResultSet result = stmt.executeQuery(runReset);
-	      this.con.commit();
-	      result.close();
-	    }
-	    catch (Exception exc)
-	    {
-	      exc.printStackTrace();
-	    }
+		{
+			stmt = con.createStatement();
+			String runReset = "CALL strengthaccelerator.Proc_ResetPersonaAccounts('" + email + "', '" + endEmail + "', '" + clientID + "');";
+			result = stmt.executeQuery(runReset);
+			this.con.commit();
+			result.close();
+		}
+		catch (Exception exc)
+		{
+			exc.printStackTrace();
+			result.close();
+		}
 	}
 	
 	/**
@@ -59,20 +69,21 @@ public class DB_Reset extends StandOut
 	 * @param endEmail	the domain name for the accounts to be reset
 	 * @param clientID	the client id for the account to be reset
 	 */
-	public void resetPersonaNoOnboarding(String email, String endEmail, String clientID)
+	public void resetPersonaNoOnboarding(String email, String endEmail, String clientID) throws SQLException
 	{
 		try
-	    {
-	      stmt = con.createStatement();
-	      String runReset = "CALL strengthaccelerator.Proc_ResetPersonaAccountsWithoutOnBoarding('" + email + "', '" + endEmail + "', '" + clientID + "');";
-	      ResultSet result = stmt.executeQuery(runReset);
-	      this.con.commit();
-	      result.close();
-	    }
-	    catch (Exception exc)
-	    {
-	      exc.printStackTrace();
-	    }
+		{
+			stmt = con.createStatement();
+			String runReset = "CALL strengthaccelerator.Proc_ResetPersonaAccountsWithoutOnBoarding('" + email + "', '" + endEmail + "', '" + clientID + "');";
+			result = stmt.executeQuery(runReset);
+			this.con.commit();
+			result.close();
+		}
+		catch (Exception exc)
+		{
+			exc.printStackTrace();
+			result.close();
+		}
 	}
 	
 	/**
@@ -81,28 +92,53 @@ public class DB_Reset extends StandOut
 	 * @param email		the email for the account to be reset
 	 * @param clientID	the client id for the account to be reset
 	 */
-	public void resetAssessment(String email, String clientID) throws Exception
+	public void resetAssessment(String email, String clientID) throws SQLException
 	{
-		try 
+		try
 		{
 			stmt = con.createStatement();
 			String deleteAccount = "CALL `strengthaccelerator`.`Proc_DeleteAccount`('" + email + "');";
-			ResultSet dA = stmt.executeQuery(deleteAccount);
+			dA = stmt.executeQuery(deleteAccount);
 			this.con.commit();
 			dA.close();
 			String createAccount = "CALL `strengthaccelerator`.`Proc_CreateTestAccount`('persona (test)', 'uno (test)', '"
 					+ email + "', 'superman', '" + clientID + "', null, null, null, 2);";
-			ResultSet cA = stmt.executeQuery(createAccount);
+			cA = stmt.executeQuery(createAccount);
 			this.con.commit();
 			cA.close();
 			String assignRole = "CALL `strengthaccelerator`.`Proc_AssignRoleToPerson`('" + email + "', 1);";
-			ResultSet aR = stmt.executeQuery(assignRole);
+			aR = stmt.executeQuery(assignRole);
 			this.con.commit();
 			aR.close();
-		} 
-		catch (Exception exc) 
+		}
+		catch (Exception exc)
 		{
 			exc.printStackTrace();
+			dA.close();
+			cA.close();
+			aR.close();
+		}
+	}
+	
+	/**
+	 * Deletes an account permanently.
+	 *
+	 * @param email		the email for the account to be reset
+	 */
+	public void deleteAccount(String email) throws SQLException
+	{
+		try
+		{
+			stmt = con.createStatement();
+			String deleteAccount = "CALL `strengthaccelerator`.`Proc_DeleteAccount`('" + email + "');";
+			dA = stmt.executeQuery(deleteAccount);
+			this.con.commit();
+			dA.close();
+		}
+		catch (Exception exc)
+		{
+			exc.printStackTrace();
+			dA.close();
 		}
 	}
 }

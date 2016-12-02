30338b78227aa0c84a49d461e0760a84f614985e
Justin Linville <jlinville01@gmail.com>
2016-12-02 10:26:11 -0800

    Update DB_Assessment.java

--- Database Objects/DB_Assessment.java
+++ Database Objects/DB_Assessment.java
@@ -20,6 +20,13 @@ public class DB_Assessment extends StandOut
 	public void runTest() throws Exception {};
 	public void tearDown() throws Exception {};
 	
+	/**
+	 * Instantiates the DB_Assessment page object.
+	 * 
+	 * @param driver	the driver for this class.
+	 * @param connection	the db connection for this class.
+	 * @throws Exception
+	 */
 	public DB_Assessment(WebDriver driver, java.sql.Connection connection) throws Exception 
 	{
 		PageFactory.initElements(driver, this);
@@ -32,34 +39,37 @@ public class DB_Assessment extends StandOut
 	 *
 	 * @param email		the email used to call the database for data.
 	 */
-	public void validateAssessment(String email) throws Exception 
+	public void validateAssessment(String email) throws SQLException 
 	{
-		stmt = this.con.createStatement();
-		String validate = "SELECT r.question_id, r.answer_id, order_label FROM response r JOIN question_answers qa ON r.answer_id = qa.answer_id JOIN person_token pt ON pt.token_id = r.token_id JOIN person p ON pt.person_id = p.uid WHERE p.email = '"
-				+ email + "';";
-		ResultSet result = stmt.executeQuery(validate);
-		this.con.commit();
+		try
+		{
+			stmt = this.con.createStatement();
+			String validate = "SELECT r.question_id, r.answer_id, order_label FROM response r JOIN question_answers qa ON r.answer_id = qa.answer_id JOIN person_token pt ON pt.token_id = r.token_id JOIN person p ON pt.person_id = p.uid WHERE p.email = '"
+					+ email + "';";
+			result = stmt.executeQuery(validate);
+			this.con.commit();
 
-		while (result.next()) {
-			// Fetch values from DB to compare
-			String question_id = result.getString("question_id");
-			String order_label = result.getString("order_label");
-
-			if (Integer.parseInt(question_id) != 14) 
+			while (result.next())
 			{
-				if (!(order_label.equals("A"))) 
+				// Fetch values from DB to compare
+				String question_id = result.getString("question_id");
+				String order_label = result.getString("order_label");
+
+				if (Integer.parseInt(question_id) = 14)
 				{
-					System.out.println(question_id + " was wrong");
+					assertThat(order_label, is("B"));
 				}
-			} 
-			else 
-			{
-				if (!(order_label.equals("B"))) 
+				else
 				{
-					System.out.println(question_id + " was wrong");
+					assertThat(order_label, is("A"));
 				}
 			}
+			result.close();
 		}
-		result.close();
+		catch (Exception exc)
+		{
+			exc.printStackTrace();
+			result.close();
+		}
 	}
 }

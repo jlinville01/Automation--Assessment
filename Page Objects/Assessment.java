fc771f4e3c97236270b58edb415e61bce316a54e
Justin Linville <jlinville01@gmail.com>
2017-02-27 09:40:15 -0800

    Update Assessment.java

--- Page Objects/Assessment.java
+++ Page Objects/Assessment.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 
+import page_objects.AbstractPageObject;
+
 public class Assessment extends AbstractPageObject
 {
 	@FindBy(id = "btnAssessment")
